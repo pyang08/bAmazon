9b9be3ee3e7fe52028c1aee75a576cd6601e83c1
pyang08 <pamelapyang@hotmail.com>
2018-06-07 23:41:49 -0500

    Update bAmazon_basic.js

--- bAmazon_basic.js
+++ bAmazon_basic.js
@@ -32,68 +32,71 @@ connection.connect(function(err) {
       })
   }
 
+  var yesToViewingInventory = function(){
+      connection.query("SELECT * FROM products", function(err,res){
+          console.log(res);
+          inquirer.prompt({
+              name:"choice",
+              type:"rawlist",
+              choices: function(value){
+                  var choiceArray = [];
+                  for(var i=0;i<res.length;i++){
+                      choiceArray.push(res[i].productname);
+                  }
+                  return choiceArray;
+              },
+              message:"What item would you like to purchase?"
 
-  
-function yesToViewingInventory() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      // once you have the items, prompt the user for which they'd like to bid on
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].item_name);
-              }
-              return choiceArray;
-            },
-            message: "What item would you like to purchase?"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].item_name === answer.choice) {
-              chosenItem = results[i];
-            }
-          }
-  
-          // determine if bid was high enough
-          if (chosenItem.stockquantity >= parseInt(answer.quantity)) {
-            // bid was high enough, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE auctions SET ? WHERE ?",
-              [
-                {
-                  stockquantity: chosenItem.stockquantity - answer.quantity
-                },
-                {
-                  id: chosenItem.id
-                }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("Congratulations! All items have been purchased. Thank you for shopping at BAMAZON!");
-                start();
-              }
-            );
-          }
-          else {
-            // bid wasn't high enough, so apologize and start over
-            console.log("Insufficient funds! Please try again.");
-            start();
-          }
-        });
-    });
+          }).then(function(answer){
+              for (var i=0;i<res.length;i++){
+                  if(res[i].productname==answer.choice){
+                      var chosenItem = res[i];
+                      inquirer.prompt({
+                          name:"purchase",
+                          type:"input",
+                          message:"How many would you like to buy?",
+                          validate: function(value){
+                              if(isNaN(value)==false){
+                                  return true;
+                              } else {
+                                  return false;
+                              }
+                          }
+                      }).then(function(answer){
+                          if(chosenItem.stockquantity >= parseInt(answer.purchase)){
+                                      purchaseItems();
+                                  } else {
+                                      console.log("Insufficient funds! Please try again.")
+                                      start();
+                                  }
+                              })
+                          }
+                    }
+                  }
+          )
+      })
   }
-  
+
+  var purchaseItems = function(){
+    connection.query('SELECT * FROM products', function(err, res) {
+    console.log(res);
+    var choiceArray = [];
+    for (var i = 0; i < res.length; i++) {
+        choiceArray.push(res[i].product_name);
+    }
+      inquirer.prompt({
+          name:"showPrice",
+          type:"rawlist",
+          message:"Great! We have enough in stock! Did you want to continue with your purchase?",
+          choices:["YES","NO"]
+      }).then(function(answer){
+          if(answer.showPrice.toUpperCase()=="YES"){
+            console.log("Congratulations! You've purchased all the items you wished for!")
+            start();
+          } else {
+              start();
+          }
+      })
+  }
+    )
+  }
